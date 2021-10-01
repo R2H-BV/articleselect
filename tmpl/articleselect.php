145329d9e5f9194881dcad3df4b9a679a8a85d67
Rick R2H <rick@r2h.nl>
2021-10-01 13:09:42 +0200

    Create list of articles with URL to article

--- tmpl/articleselect.php
+++ tmpl/articleselect.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
+
 $value = $field->value;
 
 if ($value == '')
@@ -10,9 +13,39 @@ if ($value == '')
 	return;
 }
 
-if (is_array($value))
-{
-	$value = implode(', ', $value);
+if (!function_exists('getUrl')) {
+	function getUrl ($title) {
+		$db = Factory::getDbo();
+
+		$articleId = '';
+		// Create a new query object.
+		$query = $db->getQuery(true);
+
+		$query->select($db->quoteName(array('id')));
+		$query->from($db->quoteName('#__content'));
+		$query->where($db->quotename('title') . ' LIKE ' . $db->quote($title));
+
+		// Reset the query using our newly populated query object.
+		$db->setQuery($query);
+		$articleId = $db->loadResult();
+
+		if (empty($articleId)) {
+			return 'We could not find the article id!!';
+		}
+
+		$url = Route::_('index.php?option=com_content&view=article&id='.$articleId);
+
+		return '<a class="cf-article-link" href="'.$url.'" alt="'.$title.'">'.$title.'</a>';
+	}
 }
 
-echo htmlentities($value);
+echo '<div class="cf-article-link-list">';
+if (is_array($value))
+{
+	foreach ($value as $articleTitle) {
+		echo getUrl($articleTitle) . '<br>';
+	}
+} else {
+	echo getUrl($value) . '<br>';
+}
+echo '</div>';

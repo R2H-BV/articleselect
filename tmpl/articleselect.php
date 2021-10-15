ee6c746c87738b5748758c96848d91a8d46d350d
Rick R2H <rick@r2h.nl>
2021-10-15 11:18:18 +0200

    Added classes and code cleanup

--- tmpl/articleselect.php
+++ tmpl/articleselect.php
@@ -5,23 +5,34 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
-use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
 $value = $field->value;
-
-
 
 if ($value == '')
 {
 	return;
 }
 
-$outputFormat = $field->fieldparams->get('outputFormat', 'links');
-$categories = $field->fieldparams->get('categories', '');
+$outputFormat 		= $field->fieldparams->get('outputFormat', 'links');
+$categories 		= $field->fieldparams->get('categories', '');
+$linkswrapperclass 	= $field->fieldparams->get('linkswrapperclass', '');
+$linkwrapperclass 	= $field->fieldparams->get('linkwrapperclass', '');
+$linkclass 			= $field->fieldparams->get('linkclass', '');
+
+if(strlen($linkswrapperclass)) {
+	$linkswrapperclass = ' ' . $linkswrapperclass;
+}
+
+if(strlen($linkwrapperclass)) {
+	$linkwrapperclass = ' ' . $linkwrapperclass;
+}
+
+if(strlen($linkclass)) {
+	$linkclass = ' ' . $linkclass;
+}
 
 if (!function_exists('getUrl')) {
-	function getUrl ($id, $format) {
+	function getUrl ($id, $format, $linkclasses) {
 
 		$db = Factory::getContainer()->get('DatabaseDriver');
 
@@ -47,44 +58,34 @@ if (!function_exists('getUrl')) {
 		$url = Route::_($rawurl);
 
 		if ($format == 'links') {
-			return '<a class="cf-article-link" href="'.$url.'" alt="'.$title.'">'.$title.'</a>';
+
+			return '<a class="cf-article-link'.$linkclasses.'" href="'.$url.'" alt="'.$title.'">'.$title.'</a>';
 		}
 
 		if ($format == 'array') {
 			// get custom fields values
-			$object = ['id' => $id, 'title' => $title, 'raw_url' => $rawurl, 'url' => $url, 'images' => json_decode($row[2])];
+			$object = [
+				'id' => $id,
+				'title' => $title,
+				'raw_url' => $rawurl,
+				'url' => $url,
+				'link_class' => $linkclasses,
+				'images' => json_decode($row[2])];
 
 			return $object;
-		}
-
-		if ($format == 'buttons') {
-			return '<a class="btn btn-success me-2 rounded-pill cf-article-btn-link" href="'.$url.'" alt="'.$title.'">'.$title.'</a>';
 		}
 	}
 }
 
 if ($outputFormat == 'links') {
-	echo '<div class="cf-article-link-list">';
+	echo '<div class="cf-article-link-list'.$linkswrapperclass.'">';
 	if (is_array($value))
 	{
 		foreach ($value as $articleTitle) {
-			echo getUrl($articleTitle, $outputFormat) . '<br>';
+			echo '<div class="linkwrapper'.$linkwrapperclass.'">'.getUrl($articleTitle, $outputFormat, $linkclass).'</div>';
 		}
 	} else {
-		echo getUrl($value, $outputFormat) . '<br>';
-	}
-	echo '</div>';
-}
-
-if ($outputFormat == 'buttons') {
-	echo '<div class="cf-article-button-list">';
-	if (is_array($value))
-	{
-		foreach ($value as $articleTitle) {
-			echo getUrl($articleTitle, $outputFormat);
-		}
-	} else {
-		echo getUrl($value, $outputFormat);
+		echo '<div class="linkwrapper'.$linkwrapperclass.'">'.getUrl($value, $outputFormat, $linkclass).'</div>';
 	}
 	echo '</div>';
 }
@@ -96,11 +97,11 @@ if ($outputFormat == 'array') {
 	if (is_array($value))
 	{
 		foreach ($value as $articleInfo) {
-			array_push($articleArray, getUrl($articleInfo, $outputFormat));
+			array_push($articleArray, getUrl($articleInfo, $outputFormat, $linkwrapperclass, $linkclass));
 		}
 	} else {
 
-		array_push($articleArray, getUrl($value, $outputFormat));
+		array_push($articleArray, getUrl($value, $outputFormat, $linkwrapperclass, $linkclass));
 	}
 	echo json_encode($articleArray);
 }
